30a9d13dd6b54161eaec462a577f60a3d2935c14
alfiehanks <alfiehanks88@outlook.com>
2026-05-27 00:04:01 +0100

    Added Methods to ResourceId
    Added parse(String)
    Added toString()

--- src/main/java/me/alfie/alfinolib/util/ResourceId.java
+++ src/main/java/me/alfie/alfinolib/util/ResourceId.java
@@ -9,10 +9,29 @@ import net.minecraft.resources.ResourceLocation;
 public record ResourceId(String namespace, String path) {
 
     /**
-     * Converts to Minecraft Identifier/ResourceLocation depending on version.
-     * @return
+     * @return Converts to Minecraft Identifier/ResourceLocation depending on version.
      */
     public ResourceLocation mc() {
         return ResourceLocation.fromNamespaceAndPath(namespace, path);
     }
+
+    public String toString() {
+        return namespace + ":" + path;
+    }
+
+    /**
+     * Convert a string i.e "minecraft:dirt" into a ResourceId object by splitting at the colon.
+     * @param id id as string
+     * @return ResourceId object
+     */
+    public static ResourceId parse(String id) {
+        String[] parts = id.split(":", 2);
+
+        if (parts.length != 2) throw new IllegalArgumentException("Invalid ResourceId: " + id);
+
+        return new ResourceId(parts[0], parts[1]);
+    }
+
+
 }
+
