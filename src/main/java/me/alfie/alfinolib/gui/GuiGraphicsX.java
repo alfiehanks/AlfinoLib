ee78c29956a6f115b2df44eca89f12cd60c48c1c
alfiehanks <alfiehanks88@outlook.com>
2026-05-27 17:51:49 +0100

    Updated GuiGraphicsX
    No longer extends GuiGraphics/GuiGraphicsExtractor. Now a record containing a graphics() field that returns correct class based on version

--- src/main/java/me/alfie/alfinolib/gui/GuiGraphicsX.java
+++ src/main/java/me/alfie/alfinolib/gui/GuiGraphicsX.java
@@ -7,15 +7,11 @@ import net.minecraft.client.renderer.MultiBufferSource;
 
 /**
  * Stands for GuiGraphicsX-Platform: A cross-platform, cross-version, stable call to {@code GuiGraphics} on older versions
- * and {@code GuiGraphicsExtractor} on new versions - class extends differently based on version. This is used in CommonAbstractContainerScreen
+ * and {@code GuiGraphicsExtractor} on new versions. This is used in CommonAbstractContainerScreen
  * for method params.
  *
  * @see GuiGraphicsApi
  */
-public class GuiGraphicsX extends GuiGraphics {
+public record GuiGraphicsX(GuiGraphics graphics) {
 
-
-    public GuiGraphicsX(Minecraft minecraft, MultiBufferSource.BufferSource bufferSource) {
-        super(minecraft, bufferSource);
-    }
 }
