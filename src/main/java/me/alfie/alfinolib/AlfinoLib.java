dd1fde784245eef785f467438a425db7c265e2b5
alfiehanks <alfiehanks88@outlook.com>
2026-05-30 15:16:10 +0100

    Update 1.1.5

--- src/main/java/me/alfie/alfinolib/AlfinoLib.java
+++ src/main/java/me/alfie/alfinolib/AlfinoLib.java
@@ -1,18 +1,10 @@
 package me.alfie.alfinolib;
 
 import com.mojang.logging.LogUtils;
-import me.alfie.alfinolib.datapacks.DatapackRegistry;
 import me.alfie.alfinolib.datapacks.Datapacks;
-import me.alfie.alfinolib.networking.NetworkRegisterEvent;
 import me.alfie.alfinolib.networking.Networking;
-import me.alfie.alfinolib.test.TestDatapack;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
