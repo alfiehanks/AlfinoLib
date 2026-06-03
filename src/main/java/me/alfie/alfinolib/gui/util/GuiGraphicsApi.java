70dcd493ba82b5a7652237a19c9da42a451f6b50
alfiehanks <alfiehanks88@outlook.com>
2026-06-03 12:22:19 +0100

    Text now uses Color.WHITE.getRGB()

--- src/main/java/me/alfie/alfinolib/gui/util/GuiGraphicsApi.java
+++ src/main/java/me/alfie/alfinolib/gui/util/GuiGraphicsApi.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 
+import java.awt.*;
 import java.util.List;
 
 /**
@@ -61,7 +62,7 @@ public final class GuiGraphicsApi {
      * @param dropShadow Whether or not to render the shadow behind the text (usually false in GUI screens)
      */
     public static void text(GuiGraphicsX gx, Font font, Component component, int x, int y, boolean dropShadow) {
-        gx.graphics().text(font, component, x, y, 0xFFFFFF, dropShadow);
+        gx.graphics().text(font, component, x, y, Color.WHITE.getRGB(), dropShadow);
     }
 
     /**
