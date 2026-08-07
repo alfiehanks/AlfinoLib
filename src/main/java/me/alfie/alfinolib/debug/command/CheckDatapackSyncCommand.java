faff0567d50e80386a68194fab6b11b26b7b189b
Alfie <91550583+alfiehanks@users.noreply.github.com>
2026-08-07 17:28:09 +0100

    Update CheckDatapackSyncCommand.java

--- src/main/java/me/alfie/alfinolib/debug/command/CheckDatapackSyncCommand.java
+++ src/main/java/me/alfie/alfinolib/debug/command/CheckDatapackSyncCommand.java
@@ -5,8 +5,6 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.alfie.alfinolib.AlfinoLib;
 import me.alfie.alfinolib.commands.ModCommand;
-import me.alfie.alfinolib.commands.PermissionLevel;
-import me.alfie.alfinolib.datapacks.client.ClientDatapackManager;
 import me.alfie.alfinolib.datapacks.server.ServerDatapackManager;
 import me.alfie.alfinolib.debug.RequestClientTestPacket;
 import me.alfie.alfinolib.debug.datapack.TestDatapack;
